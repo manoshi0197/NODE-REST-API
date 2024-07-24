022b052c338a6cef2b598a3a747b297622606ce5
manoshi <manoshi0197@gmail.com>
2024-07-23 21:46:27 -0400

    Encrypting Password

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-//REGISTER
-router.post("/register", async(req, res) => {
-    const user = await new User({
-        username:"manoshi",
-        email: "manoshinath17@gmail.com",
-        password:"123456"
-    })
-    //save this user
-    await user.save(); 
-    res.send("ok");
+// Example GET route
+// router.get('/', (req, res) => {
+//     res.send('Auth route is working');
+// });
+
+// REGISTER
+router.post("/register", async (req, res) => {
+    try {
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword =await bcrypt.hash(req.body.password, salt);
+        //create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        //save user and return response
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Error registering user", error: err });
+    }
 });
 
 module.exports = router;
