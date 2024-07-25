9456716913174a087aa0d9d1ade6a76197477bc3
manoshi <manoshi0197@gmail.com>
2024-07-25 01:00:20 -0400

    Creating user login route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
+const User = require("../models/User");
 const express = require('express');
 const router = express.Router();
 //user get data, post and everything we do in this page
-router.get("/",(req,res)=>{
-    res.send("hey its user routes");
-});
+// router.get("/",(req,res)=>{
+//     res.send("hey its user routes");
+// });
+
+//update user
+
+//Delete user
+
+//get a user
+
+//follow a user
+
+//unfollow a user
 
 //to use it in the index fle we export it
 module.exports = router;
